8e4943d3de961efb73a80d0b87e5a5f554d3c227
zijie.zeng <jeffreyzzj1996@163.com>
2020-05-03 23:39:59 +0800

    zijie.zeng add

--- src/main/game/ZeroToHundred.java
+++ src/main/game/ZeroToHundred.java
@@ -9,7 +9,8 @@ import java.util.Scanner;
  * @create: 2020-05-03 23:16
  */
 public class ZeroToHundred {
-    private static final String str = "请输入%s到%s之间的数";
+    private static final String str1 = "请输入%s到%s之间的数";
+    private static final String str2 = "请重新输入%s到%s之间的数";
     public static void main(String[] args) {
         int target = (int) (Math.random() * 100);
         int left = 0;
@@ -17,6 +18,10 @@ public class ZeroToHundred {
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
         while (target != input) {
+            if (input<=left || input>=right) {
+                System.out.println(String.format(str2, left, right));
+                continue;
+            }
             if (input < target) {
                 left = input;
             }
@@ -24,7 +29,7 @@ public class ZeroToHundred {
                 right = input;
             }
 
-            System.out.println(String.format(str, left, right));
+            System.out.println(String.format(str1, left, right));
             input = sc.nextInt();
         }
         System.out.println("over, the target number is " + target);
